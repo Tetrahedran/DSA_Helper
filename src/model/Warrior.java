1768aff27fbad86910f970456d7b86ab706ce324
Tetrahedran <paddy38@web.de>
2017-12-10 11:26:38 +0100

    primitive warrior

---
+++ src/model/Warrior.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a Warrior
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public class Warrior extends Fighter {
+
+	public Warrior(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
