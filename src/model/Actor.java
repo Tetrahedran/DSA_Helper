1dcc46a85d26737ec8c402ae27711e815634511b
Tetrahedran <paddy38@web.de>
2017-12-10 11:21:40 +0100

    primitive Actor

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Actor {
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * this class represents an actor in DSA
+ * @author Tetrahedran
+ * @version 0.1
+ */
+@SuppressWarnings("unused")
+public abstract class Actor {
+	private static List<Actor> allActors = new LinkedList<>();
+	private final String NAME;
+	private final int AGE;
+
+
+	protected Actor(String NAME, int AGE) {
+		this.NAME = NAME;
+		this.AGE = AGE;
+		Actor.allActors.add(this);
+	}
+
+	public static List<Actor> getAllActors() {
+		return allActors;
+	}
+
+	public String getNAME() {
+		return NAME;
+	}
+
+	public int getAGE() {
+		return AGE;
+	}
 }
