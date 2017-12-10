b01fdd68f4ea3df0695c3310610c92f1a094a91e
Tetrahedran <paddy38@web.de>
2017-12-10 11:31:27 +0100

    primitive thief

---
+++ src/model/Thief.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a thief
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public class Thief extends Fighter {
+
+	public Thief(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
