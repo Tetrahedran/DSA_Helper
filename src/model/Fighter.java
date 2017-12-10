72d92f020d8b47178befe1d84076d4844794d95a
Tetrahedran <paddy38@web.de>
2017-12-10 11:24:16 +0100

    primitive fighter

---
+++ src/model/Fighter.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a fighter
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public abstract class Fighter extends Actor {
+
+	public Fighter(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
