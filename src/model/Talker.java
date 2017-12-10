e199b592ad19534dad47693193bd65c7063ed850
Tetrahedran <paddy38@web.de>
2017-12-10 11:25:20 +0100

    primitive talker

---
+++ src/model/Talker.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * this class represents a person you only can talk to
+ */
+public class Talker extends Actor{
+
+	public Talker(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
