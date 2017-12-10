77d24e8997a6021224b1f45478a2861382406487
Tetrahedran <paddy38@web.de>
2017-12-10 11:29:10 +0100

    primitive priest

---
+++ src/model/Priest.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a priest
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public class Priest extends Fighter {
+
+	public Priest(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
