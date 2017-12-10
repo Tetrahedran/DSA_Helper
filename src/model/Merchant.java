2616bab204e0737955654425fa9533b98d1d81b7
Tetrahedran <paddy38@web.de>
2017-12-10 11:22:51 +0100

    primitive Merchant

---
+++ src/model/Merchant.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a merchant
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public class Merchant extends Actor {
+
+	public Merchant(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
