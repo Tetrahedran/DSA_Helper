8da7a6cf4dc6740f9c16cfa340a60f21fb18955f
Tetrahedran <paddy38@web.de>
2017-12-10 11:28:08 +0100

    primitive magician

---
+++ src/model/Magician.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * this class represents a Magician
+ * @author Tetrahedran
+ * @version 0.1
+ */
+public class Magician extends Fighter {
+
+	public Magician(String NAME, int AGE) {
+		super(NAME, AGE);
+	}
+}
