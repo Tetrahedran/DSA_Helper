5341881bd4195b2d47b3fe8e308f534ef6c4d1f8
Tetrahedran <paddy38@web.de>
2017-12-14 22:40:04 +0100

    added inventory to actor

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -13,13 +13,17 @@ public abstract class Actor {
 	private static List<Actor> allActors = new LinkedList<>();
 	private final String NAME;
 	private final int AGE;
+	private List<InventoryObject> inventory;
 
 
 	protected Actor(String NAME, int AGE) {
 		this.NAME = NAME;
 		this.AGE = AGE;
+		inventory = new LinkedList<>();
 		Actor.allActors.add(this);
 	}
+
+	//getter methods
 
 	public static List<Actor> getAllActors() {
 		return allActors;
@@ -32,4 +36,29 @@ public abstract class Actor {
 	public int getAGE() {
 		return AGE;
 	}
+
+	public List<InventoryObject> getInventory() {
+		return inventory;
+	}
+
+	//Setter methods
+
+	public void addToInventory(InventoryObject object){
+		inventory.add(object);
+	}
+
+	public void addToInventory(List<InventoryObject> objects){
+		inventory.addAll(objects);
+	}
+
+	//other methods
+
+	public boolean removeFromInventory(InventoryObject object){
+		return inventory.remove(object);
+	}
+
+	public boolean removeFromInventory(List<InventoryObject> objects){
+		return inventory.removeAll(objects);
+	}
+
 }
