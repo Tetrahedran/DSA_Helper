b7d6178142e26e0eea35d81d11265baa79cb6b99
Tetrahedran <paddy38@web.de>
2017-12-14 22:18:11 +0100

    added main class

---
+++ src/controll/Main.java
@@ -0,0 +1,16 @@
+package controll;
+
+import view.MainWindow;
+
+public class Main {
+
+
+	private Main() {
+	}
+
+	public static void main(String[] args){
+		MainWindow window = new MainWindow();
+	}
+}
+
+
